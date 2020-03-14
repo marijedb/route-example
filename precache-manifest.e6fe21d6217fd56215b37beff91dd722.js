92a0df17e313eec822a0f8029c1c18a5c7b3bb3e
Marije <49620465+marijedb@users.noreply.github.com>
2020-03-14 18:23:41 +0100

    Updates

---
+++ precache-manifest.e6fe21d6217fd56215b37beff91dd722.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9616cbf1032e7590db6f0474e040b04f",
+    "url": "/route-example/index.html"
+  },
+  {
+    "revision": "6503b81783007f5c7c0c",
+    "url": "/route-example/static/css/main.eb9a94bd.chunk.css"
+  },
+  {
+    "revision": "09563577d35bf5d9d663",
+    "url": "/route-example/static/js/2.ec046201.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/route-example/static/js/2.ec046201.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6503b81783007f5c7c0c",
+    "url": "/route-example/static/js/main.74fecc77.chunk.js"
+  },
+  {
+    "revision": "cd28e392370ae5d4726d",
+    "url": "/route-example/static/js/runtime-main.ae2f79b0.js"
+  }
+]);
